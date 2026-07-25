9800a6fc692b38ca6960dba45a0cc69fe3005e7c
Salvador Cruz <salvador10@ucla.edu>
2026-07-25 09:13:07 -0700

    Got the LCD set up!
    The LCD is now properly wired up with the ESP32 and the code has initialized it so its ready for the next step.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <LiquidCrystal.h>
 //imports the confidential file in order to get the wifi name and password
 #include "confidential.h"
 
+const int RS_PIN = 27;
+//for now we have R/W pin set to GND since we only plan to write 
+const int E_PIN = 14;
+//we'll be using the simpler 4-bit method do D0-D3 will be disconnected
+const int D4 = 32;
+const int D5 = 33;
+const int D6 = 25;
+const int D7 = 26;
+
+LiquidCrystal lcd(RS_PIN, E_PIN, D4, D5, D6, D7);
 
 void setup() 
-{
+{ 
+    //setting up the LCD 
+    lcd.begin(16,2);
+    lcd.print("hi there");
+
     //setting up the esp32 to connect to an existing wifi router
     WiFi.mode(WIFI_STA);
     //starts wifi connection
@@ -17,17 +32,15 @@ void setup()
     //Gives the serial connection a small delay to initialize
     delay(1000);
 
-    Serial.println();
-    Serial.println("Esp32 connection succesful");
-}
-
-void loop()
-{
-    Serial.println("The ESP32 is running");
+    //just a little code to make sure ik wifi is up
     if (WiFi.status() == WL_CONNECTED)
     {
         Serial.println("Wifi Successfully Connected");
     }
+}
 
-    delay(1000);
+void loop()
+{
+
+
 }
